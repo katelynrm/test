c03f04571fd72ba859ff3f63ee9fa6a757008aa3
katelynrm <37051008+katelynrm@users.noreply.github.com>
2018-03-05 17:50:39 -0700

    Created list of months

---
+++ average_temps.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 17:45:20 2018
+
+@author: Katelyn
+
+Exercise 2
+"""
+
+monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', \
+             'August', 'September', 'October', 'November', 'December']
