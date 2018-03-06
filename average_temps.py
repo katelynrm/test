d6e17c1c941cae81663802d2f8f2dad241515a09
katelynrm <37051008+katelynrm@users.noreply.github.com>
2018-03-05 17:55:39 -0700

    added temp list and comments

--- average_temps.py
+++ average_temps.py
@@ -6,6 +6,10 @@ Created on Mon Mar  5 17:45:20 2018
 
 Exercise 2
 """
-
+#A list of the 12 months
 monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', \
              'August', 'September', 'October', 'November', 'December']
+
+#A list of the average temps in C for each month 
+aveTemp = [-3.5, -4.5, -1.0, 4.0, 10.0, 15.0, 18.0, 16.0, 11.5, 6.0, 2.0, -1.5]
+
