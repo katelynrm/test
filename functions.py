ec9823cb7bbd11e2bf068f4518c0b5ecf0a0551b
katelynrm <37051008+katelynrm@users.noreply.github.com>
2018-03-22 16:22:16 -0600

    Initial

---
+++ functions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 22 16:14:44 2018
+
+@author: katel
+"""
+
+def fahrToCelsius(tempFahrenheit0):
+    '''Converts temp in Fahr to temp in celcius 
+    Parameters
+    ---------
+    
