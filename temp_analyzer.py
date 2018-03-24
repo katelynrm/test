08a17cc444d153ed37ba3f91fb867d04dab21771
katelynrm <37051008+katelynrm@users.noreply.github.com>
2018-03-24 11:33:28 -0600

    added some fixes

--- temp_analyzer.py
+++ temp_analyzer.py
@@ -5,5 +5,13 @@ Created on Sat Mar 24 11:00:54 2018
 @author: katel
 """
 
-import functions
-import data
+from functions import fahrToCelsius as fTC
+from functions import tempClassifier as tC
+from data import tempData as tD
+
+#created an empty list that will eventually contain classified values
+tempClasses = []
+
+#iterate over the Fahrenheit temps in the tempData list
+for temp in tD:
+    
