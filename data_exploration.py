01134affe884fc0180ea11e560dc77e6c60a3ff6
katelynrm <37051008+katelynrm@users.noreply.github.com>
2018-03-27 20:17:51 -0600

    read cvs in, imported pandas

---
+++ data_exploration.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 24 13:53:57 2018
+
+@author: katel
+
+Exercise 5: Manipulating data with Pandas
+"""
+import pandas as pd
+
+dataFrame = pd.read_csv('6153237444115dat.csv')
+
