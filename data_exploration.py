a1a8f005f9832063d2e8e8853084e7ffe3ee85d9
katelynrm <37051008+katelynrm@users.noreply.github.com>
2018-03-27 20:36:18 -0600

    Print column names and number of rows

--- data_exploration.py
+++ data_exploration.py
@@ -11,3 +11,6 @@ import pandas as pd
 #Reading in csv and assigning na values as an astrisk
 data = pd.read_csv('6153237444115dat.csv', na_values=['*', '**', '***', '****', '*****', '******'])
 
+print(data.columns)
+
+print (len(data.index))
